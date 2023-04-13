e4026f660c6118d4398075b6f0e89496ef960872
PhoebaRepo <phoebabraham04@gmail.com>
2023-04-13 08:40:40 +0530

    Demo is committed

---
+++ src/test/java/sample/Demo.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void demo() {
+		// TODO Auto-generated method stub
+    System.out.println("Demo");
+	}
+
+}
